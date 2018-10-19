dce517a42419eeebbb17df9eb9752e5ff16aa279
Martin Pach <m.pach97@gmail.com>
2018-10-19 14:44:59 +0200

    Disabling login button while validating credentials

--- src/app/admin/containers/login/login.component.ts
+++ src/app/admin/containers/login/login.component.ts
@@ -1,9 +1,4 @@
-import {
-  Component,
-  OnInit,
-  ChangeDetectionStrategy,
-  ChangeDetectorRef
-} from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ChangeDetectorRef } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AdminAuthService } from '../../services/admin-auth.service';
@@ -16,30 +11,31 @@ import { AdminAuthService } from '../../services/admin-auth.service';
 })
 export class LoginComponent implements OnInit {
   areCredentialsValid: boolean = true;
+  loginInProcess: boolean = false;
 
-  constructor(
-    private adminAuth: AdminAuthService,
-    private router: Router,
-    private cd: ChangeDetectorRef
-  ) {}
+  constructor(private adminAuth: AdminAuthService, private router: Router, private cd: ChangeDetectorRef) {}
 
   ngOnInit() {}
 
   onSubmit(form: NgForm) {
+    this.loginInProcess = true;
+    this.areCredentialsValid = true;
+
     if (!form.value.email || !form.value.password) {
+      this.loginInProcess = false;
       return (this.areCredentialsValid = false);
     }
-    this.adminAuth
-      .login(form.value.email, form.value.password)
-      .subscribe(isVerified => {
-        if (isVerified) {
-          this.areCredentialsValid = true;
-          form.reset();
-          this.router.navigate(['admin']);
-        } else {
-          this.areCredentialsValid = false;
-        }
-        this.cd.markForCheck();
-      });
+    this.adminAuth.login(form.value.email, form.value.password).subscribe(isVerified => {
+      if (isVerified) {
+        this.areCredentialsValid = true;
+        form.reset();
+        this.router.navigate(['admin']);
+      } else {
+        this.areCredentialsValid = false;
+      }
+
+      this.loginInProcess = false;
+      this.cd.markForCheck();
+    });
   }
 }
