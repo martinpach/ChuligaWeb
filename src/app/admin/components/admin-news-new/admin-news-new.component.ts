0cc9562ce90c0fd729135f7d91aa53719c346ee4
Martin Pach <m.pach97@gmail.com>
2018-11-09 15:51:08 +0100

    New newsitem component 2nd commit

--- src/app/admin/components/admin-news-new/admin-news-new.component.ts
+++ src/app/admin/components/admin-news-new/admin-news-new.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { FileService } from '../../../shared/services/files.service';
+import { NewsService } from '../../../shared/services/news.service';
+import { NewsItem, ImageInfo } from '../../../shared/models';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-admin-news-new',
@@ -7,6 +12,12 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AdminNewsNewComponent implements OnInit {
+  description: string;
+  imageInfo: ImageInfo;
+  isLoading = false;
+  isValid: boolean;
+  isError: boolean;
+
   wysiwygOptions = {
     editable: true,
     height: 'auto',
@@ -23,7 +34,41 @@ export class AdminNewsNewComponent implements OnInit {
     ]
   };
 
-  constructor() {}
+  constructor(private fileService: FileService, private newsService: NewsService, private router: Router) {}
 
   ngOnInit() {}
+
+  onImageClick() {
+    this.fileService.delete(this.imageInfo.name);
+    this.imageInfo = undefined;
+  }
+
+  onImageUploaded(imageInfo: ImageInfo) {
+    this.imageInfo = imageInfo;
+  }
+
+  onSubmit(f: NgForm) {
+    this.isError = false;
+    if (!f.value.heading || !this.description) {
+      this.isValid = false;
+      return;
+    }
+    this.isValid = true;
+
+    const newsItem: NewsItem = {
+      heading: <string>f.value.heading,
+      description: <string>this.description,
+      picture: <ImageInfo>this.imageInfo || { name: '', url: '' },
+      shortDescription: <string>f.value.shortDescription || '',
+      date: new Date()
+    };
+    this.isLoading = true;
+    this.newsService
+      .addNewsItem(newsItem)
+      .then(() => {
+        this.router.navigate(['admin', 'news', 'list']);
+        this.isLoading = false;
+      })
+      .catch(() => (this.isError = true));
+  }
 }
