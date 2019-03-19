5219050279efd176257e0c43a8e637d60bb36e3b
Martin Pach <m.pach97@gmail.com>
2019-03-19 19:48:14 +0100

    Fix for addAtributeToIframe

--- src/app/admin/utils/index.ts
+++ src/app/admin/utils/index.ts
@@ -1,5 +1,7 @@
 export const addAttributeToIframe = (html: string, attribute: string): string => {
-  const position = html.indexOf('iframe') + 6;
+  const iframePosition = html.indexOf('iframe');
+  if (iframePosition < 0) return html;
+  const position = iframePosition + 6;
 
   return [html.slice(0, position), attribute, html.slice(position)].join('');
 };
